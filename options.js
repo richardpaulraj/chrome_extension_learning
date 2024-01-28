27275ab363ce59902cd873ae729b9bfc78897c42
Richard Paul <richardpaulraj2@gmail.com>
2024-01-28 23:18:37 +0530

    Learning about options page in chrome extension

---
+++ options.js
@@ -0,0 +1,8 @@
+const nameInput = document.getElementById("name-input");
+const saveBtn = document.getElementById("save-btn");
+
+saveBtn.addEventListener("click", () => {
+  if (nameInput.value) {
+    const name = nameInput.value;
+  }
+});
